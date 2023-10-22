b7bf09599594aab39dc5d3701148f7a43a2b411a
Igor Rončević <57319163+igorroncevic@users.noreply.github.com>
2023-10-22 15:12:52 +0200

    minor bug idk

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -15,7 +15,6 @@ const AdoraApp = ({ Component, pageProps }: AppProps) => {
   if (typeof window !== "undefined") {
     localStorage.setItem("lit-web3-provider", "metamask");
   }
-  
   return (
     <WagmiConfig config={wagmiConfig}>
       <NextNProgress />
